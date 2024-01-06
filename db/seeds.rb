9ae54d7e954eb9eada9002e047d6876c2493acc4
Louis Hocquet <louis.hocquet@sfr.fr>
2024-01-06 12:17:47 +0100

    Add section behavior and global padding sass variables

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,9 @@
 Activity.destroy_all
 Activity.create(
   { name: '59 Rivoli',
-    description: 'Open art studios, à l\'origine c\'est un squat artistique',
+    description: "Open art studios, à l'origine c'est un squat artistique. Tout commence en 99, lorsqu’une dizaine d’artistes décide de squater un immeuble abandonné par le Crédit Lyonnais. L'objectif : transformer l'endroit en lieu de création artistique alternatif.
+
+    C'est un lieu hybride, unique à Paris. En plus d’accueillir des artistes en résidence, le 59 Rivoli propose des expos régulières, des concerts, des rencontres avec des artistes et bien sûr des visites gratuites et libres de ses ateliers tous les après-midi",
     address: '59 rue de Rivoli, 75001 Paris',
     google_maps_url: 'https://maps.app.goo.gl/Q1Wq2mbyYE9KXiya9' }
 )
